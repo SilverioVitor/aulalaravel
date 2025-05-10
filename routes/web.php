7f9f1797c2223693dc4ec966c1b45bb711835276
Silvério Vitor <silveriovs54@gmail.com>
2025-05-09 21:23:58 -0300

    aula routes com css

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/kingdom', function () {
+    return view('kingdom');
+});
+Route::get('/king', function () {
+    return view('king');
+});
 Route::get('/', function () {
     return view('welcome');
 });
